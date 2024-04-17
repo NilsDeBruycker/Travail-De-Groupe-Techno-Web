c8635d2832fba0797ea6083e3580c04973478373
lydia-messaoudi <160009996+lydia-messaoudi@users.noreply.github.com>
2024-04-17 14:41:30 +0200

    changed services/users

--- TP4devTravG-/tp04_librairie/app/services/users.py
+++ TP4devTravG-/tp04_librairie/app/services/users.py
@@ -3,56 +3,80 @@ from app.schemas import UserSchema
 from sqlalchemy.orm import Session
 from app.models.book import User  
 from sqlalchemy import select
+
+
 def get_user_by_username(username: str):
-    for user in database['users']:
-        if user['username'] == username:
-            return UserSchema.model_validate(user)
+    with Session() as session:
+        statement = select(User).filter_by(username=username)
+        user = session.scalar(statement) 
+        if user is not None:
+            return UserSchema(
+                id=user.id,
+                username=user.username,
+                password=user.password,
+            )
     return None
 
 
 def get_user_by_email(email: str):
-    for user in database['users']:
-        if user["email"] == email:
-            return UserSchema.model_validate(user)
+    with Session() as session:
+        statement = select(User).filter_by(email=email)
+        user = session.scalar(statement) 
+        if user is not None:
+            return UserSchema(
+                id=user.id,
+                email=user.email,
+                password=user.password,
+            )
     return None
 
+
 def sign_up_user(new_user):
-    database["users"].append(new_user)
+    with Session() as session:
+       session.add(new_user)
+       session.commit()
     return new_user
 
-def get_all_users() -> list[UserSchema]:
-    user_data = database["users"]
-    books = [UserSchema.model_validate(user) for user in user_data]
 
-    return books
 
-def block_user(email):
-    i=0
-    for user in database["users"]:
-        if  database["users"][i]["email"]== email:
-            database["users"][i]["blocked"]=True
-        i+=1
+def get_all_users(session: Session) :
+    users = session.query(User).all()
 
-def unblock_user(email):
-    i=0
-    for user in database["users"]:
-        if  database["users"][i]["email"]== email:
-            database["users"][i]["blocked"]=False
-        i+=1
+    return users
 
-def promote_user(email: str) -> UserSchema:
-    i=0
-    for user in database["users"]:
-        if  database["users"][i]["email"]== email:
-            database["users"][i]["role"]="admin"
-        i+=1
 
-def demote_user(email: str) -> UserSchema:
-    i=0
-    for user in database["users"]:
-        if  database["users"][i]["email"]== email:
-            database["users"][i]["role"]="normal"
-        i+=1
+
+def block_user(session: Session, email: str):
+    user = session.query(User).filter(User.email == email).first()
+    if user:
+        user.blocked = True
+        session.commit()
+
+
+def ublock_user(session: Session, email: str):
+    user = session.query(User).filter(User.email == email).first()
+    if user:
+        user.blocked = False
+        session.commit()
+
+
+def promote_user(session: Session, email: str) -> User:
+    user = session.query(User).filter(User.email == email).first()
+    if user:
+        user.role = "admin"
+        session.commit()
+
+    return user
+
+
+def demote_user(session: Session, email: str) -> User:
+    user = session.query(User).filter(User.email == email).first()
+    if user:
+        user.role = "normal"
+        session.commit()
+
+    return user
+
 def delete_user_by_id( user_id: str):
     with Session as session:
         statement = select(User).filter_by(id=user_id)
